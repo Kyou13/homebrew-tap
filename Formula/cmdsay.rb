4612cc321084a99d01f8a253bd6d5d67e4da61e8
Kyou13 <kyou1013@gmail.com>
2019-12-08 16:29:09 +0900

    [update] buildするように

--- Formula/cmdsay.rb
+++ Formula/cmdsay.rb
@@ -4,12 +4,15 @@
 class Cmdsay < Formula
   desc ""
   homepage ""
-  url "https://github.com/Kyou13/cmdsay/releases/download/0.0.1/cmdsay"
+  url "https://github.com/Kyou13/cmdsay/archive/v0.0.1.tar.gz"
   sha256 "f01a29f84166a87760fc5efa102b7e01db1254444bc3f9510e2dbc27614ae436"
 
   depends_on "cowsay"
+  depends_on "shc"
 
   def install
+    print buildpath
+    system "shc", "-f", "cmdsay.sh", "-o", "cmdsay"
     bin.install "cmdsay"
   end
 end
